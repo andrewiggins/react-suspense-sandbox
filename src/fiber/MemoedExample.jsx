ad187dea3380bf221f24d595cf1ca8c0a5e7f9a7
Andre Wiggins <andwi@microsoft.com>
2021-08-12 18:19:57 -0700

    Add MemoedExample explanation

--- src/fiber/MemoedExample.jsx
+++ src/fiber/MemoedExample.jsx
@@ -1,6 +1,31 @@
 import * as React from "react";
 import { useState, useMemo } from "react";
 
+/* ==========================
+
+  Demo to show how Hoisting JSX elements and wrapping JSX in useMemo is effective
+  in preventing renders, similarly to using React.memo.
+
+  The Value component will log to the console every time it renders. Notice how
+  while all instances of Value rerender on "increment count" click, only the
+  Normal and MemoedProps Value rerender when the "rerender" button is clicked.
+
+  React recognizes that the JSX element and its props haven't changed and so it
+  doesn't rerender the Hoisted or Memoed component.
+
+  Likely this line in `beginWork` (and subsequent if statements) combine to enable
+  this bailout:
+  https://github.com/facebook/react/blob/e4e8226c625220944819c9237346569f09ad90a9/packages/react-reconciler/src/ReactFiberBeginWork.new.js#L3623
+
+  It appears React checks for props to have changed (and types in Dev mode),
+  context, or a "scheduled update"? to trigger a render
+
+  `attemptEarlyBailoutIfNoScheduledUpdate` does some bookkeeping and
+  `bailoutOnAlreadyFinishedWork` verifies children don't have any scheduled work
+  on a lane before returning `null` and skipping the entire subtree.
+
+ ========================== */
+
 function Value({ label, value }) {
   console.log("rerendering", label);
   return (
@@ -31,6 +56,7 @@ export function MemoedExample() {
 
   return (
     <div>
+      <h2>Memo'ed component example</h2>
       <Value label="Normal" value={count} />
       {MemoedValue}
       {/* Note: This doesn't work because createElement copies the props onto a new object each time it is called */}
