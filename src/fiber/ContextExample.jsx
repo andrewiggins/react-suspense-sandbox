49a23588590cea4e6e89924c230aedf4dff50fb7
Andre Wiggins <andwi@microsoft.com>
2021-08-13 00:45:08 -0700

    Update ContextExample with an explanation

--- src/fiber/ContextExample.jsx
+++ src/fiber/ContextExample.jsx
@@ -2,6 +2,26 @@ import * as React from "react";
 
 const { Fragment } = React;
 
+/*
+  This example demonstrates how React will not call render on components between
+  a Context.Provider and Context.Consumer but still traverses down the tree
+  performing work.
+
+  Examining the logs of pressing Toggle show that "rendering MyChild" and
+  "rendering Time" are never logged when the only update is a Provider change,
+  but the ReactTracer still calls "performUnitOfWork" on fibers between the
+  Provider and Consumer. Most of these intermediate fibers bailout since props
+  haven't changed (1). Fibers that aren't between the Provider and Consumer
+  return null in beginWork so their subtree is skipped (no child update is
+  scheduled) (2) while Fibers between the Provider and Consumer do return their
+  child since one of their children has an update scheduled (3).
+
+  (1) https://github.com/facebook/react/blob/b4f119cdf1defe2437e00022f670c6ef818cd43f/packages/react-reconciler/src/ReactFiberBeginWork.new.js#L3252
+  (2) https://github.com/facebook/react/blob/b4f119cdf1defe2437e00022f670c6ef818cd43f/packages/react-reconciler/src/ReactFiberBeginWork.new.js#L3140
+  (3) https://github.com/facebook/react/blob/b4f119cdf1defe2437e00022f670c6ef818cd43f/packages/react-reconciler/src/ReactFiberBeginWork.new.js#L3147
+*/
+
+
 const ctx = React.createContext(null);
 
 class MyProvider extends React.Component {
@@ -15,6 +35,7 @@ class MyProvider extends React.Component {
   }
 
   render() {
+    console.log("rendering MyProvider");
     return (
       <Fragment>
         <div>
@@ -28,9 +49,13 @@ class MyProvider extends React.Component {
   }
 }
 
-const Time = () => <div>{Date.now()}</div>;
+const Time = () => {
+  console.log('Rendering Time');
+  return <div>{Date.now()}</div>;
+};
 
 const MyChild = () => {
+  console.log('rendering MyChild');
   return (
     <Fragment>
       {Date.now()}
@@ -46,6 +71,7 @@ const MyChild = () => {
 };
 
 export function ContextExample() {
+  console.log('rendering ContentExample');
   return (
     <Fragment>
       <h2>Context Example</h2>
