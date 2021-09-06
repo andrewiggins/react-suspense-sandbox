93d49588ba0a4be097fc70c9db2bd09942b5536b
Andre Wiggins <andrewiggins@live.com>
2021-09-06 12:54:17 -0700

    Deploy gh-pages on push to main

--- deploy.js
+++ deploy.js
@@ -1,27 +1,35 @@
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
-import del from "del";
-import cpy from "cpy";
+import ghPages from "gh-pages";
 
 // @ts-ignore
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const p = (...args) => join(__dirname, ...args);
 
 async function main() {
-  await del(
-    [
-      "jsconfig.json",
-      "deploy.js",
-      "lib",
-      "src",
-      "assets/react-method-tracing*",
-    ],
-    {
-      cwd: p("dist"),
-    }
-  );
+  let user;
+  if (process.env.GITHUB_WORKFLOW) {
+    user = {
+      name: "github-actions-bot",
+      email: "support+actions@github.com",
+    };
+  }
 
-  await cpy(["src/**/*.html"], "dist", { cwd: p(), parents: true });
+  ghPages.publish(
+    p("dist"),
+    {
+      src: [
+        "index.html",
+        "*.js",
+        "assets/**/*",
+        "chunks/**/*",
+        "src/**/index.html",
+        "!**/react-method-tracing*",
+      ],
+      user,
+    },
+    () => ghPages.clean()
+  );
 }
 
 main();
