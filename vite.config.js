368220df46b8a3125736f274e756c34d7040ab46
Andre Wiggins <andrewiggins@live.com>
2023-01-09 03:28:30 -0800

    Fix vite config for children page

--- vite.config.js
+++ vite.config.js
@@ -11,31 +11,32 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const expand = (...args) => path.join(__dirname, ...args);
 
 export default defineConfig({
-  base: "/react-suspense-sandbox/",
-  plugins: [
-    react({
-      jsxRuntime: "classic",
-      fastRefresh: false,
-    }),
-  ],
-  resolve: {
-    alias: {
-      react: expand(`./lib/react.${reactCommit}.${buildType}.js`),
-      "react-dom": expand(`./lib/react-dom.${reactCommit}.${buildType}.js`),
-      scheduler: expand(`./lib/scheduler.${reactCommit}.${buildType}.js`),
-    },
-  },
-  build: {
-    minify: false,
-    sourcemap: true,
-    rollupOptions: {
-      input: {
-        main: expand("index.html"),
-        fiber: expand("src/fiber/index.html"),
-        fragments: expand("src/fragments/index.html"),
-        movieApp: expand("src/movie-app/index.html"),
-        // suspense: expand("src/suspense/index.html"),
-      },
-    },
-  },
+	base: "/react-suspense-sandbox/",
+	plugins: [
+		react({
+			jsxRuntime: "classic",
+			fastRefresh: false,
+		}),
+	],
+	resolve: {
+		alias: {
+			react: expand(`./lib/react.${reactCommit}.${buildType}.js`),
+			"react-dom": expand(`./lib/react-dom.${reactCommit}.${buildType}.js`),
+			scheduler: expand(`./lib/scheduler.${reactCommit}.${buildType}.js`),
+		},
+	},
+	build: {
+		minify: false,
+		sourcemap: true,
+		rollupOptions: {
+			input: {
+				main: expand("index.html"),
+				children: expand("src/children/index.html"),
+				fiber: expand("src/fiber/index.html"),
+				fragments: expand("src/fragments/index.html"),
+				movieApp: expand("src/movie-app/index.html"),
+				// suspense: expand("src/suspense/index.html"),
+			},
+		},
+	},
 });
