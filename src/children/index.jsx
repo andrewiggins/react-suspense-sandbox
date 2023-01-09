93784bf07191c8f059f557b72ae12c5db8a8b8ed
Andre Wiggins <andrewiggins@live.com>
2023-01-09 03:25:39 -0800

    Add ideal mutation display

--- src/children/index.jsx
+++ src/children/index.jsx
@@ -27,6 +27,7 @@ function List({ values }) {
  * @property {string} label
  * @property {React.ReactElement} init
  * @property {React.ReactElement} runs
+ * @property {string[]} idealDom
  */
 
 /**
@@ -50,78 +51,97 @@ const operations = [
 				<li key="2">z</li>
 			</ul>
 		),
+		idealDom: [],
 	},
 	{
 		id: "append",
 		label: "Append new item",
 		init: <List values={["a", "b", "c"]} />,
 		runs: <List values={["a", "b", "c", "d"]} />,
+		idealDom: ["<ol>ab.insertBefore(<li>c, Null)"],
 	},
 	{
 		id: "remove-last",
 		label: "Remove last item",
 		init: <List values={["a", "b", "c", "d"]} />,
 		runs: <List values={["a", "b", "c"]} />,
+		idealDom: ["<li>d.remove()"],
 	},
 	{
 		id: "prepend",
 		label: "Prepend new item",
 		init: <List values={["b", "c"]} />,
 		runs: <List values={["a", "b", "c"]} />,
+		idealDom: ["<ol>bc.insertBefore(<li>a, <li>b)"],
 	},
 	{
 		id: "remove-first",
 		label: "Remove first item",
 		init: <List values={["z", "a", "b", "c"]} />,
 		runs: <List values={["a", "b", "c"]} />,
+		idealDom: ["<li>z.remove()"],
 	},
 	{
 		id: "insert-middle",
 		label: "Insert new item in middle",
 		init: <List values={["a", "c"]} />,
 		runs: <List values={["a", "b", "c"]} />,
+		idealDom: ["<ol>ac.insertBefore(<li>b, <li>c)"],
 	},
 	{
 		id: "remove-middle",
 		label: "Remove items from middle",
 		init: <List values={["a", "b", "x", "y", "z", "c", "d"]} />,
 		runs: <List values={["a", "b", "c", "d"]} />,
+		idealDom: ["<li>z.remove()", "<li>y.remove()", "<li>x.remove()"],
 	},
 	{
 		id: "move-one-to-begin",
 		label: "Move one to beginning",
 		init: <List values={["b", "c", "d", "a"]} />,
 		runs: <List values={["a", "b", "c", "d"]} />,
+		idealDom: ["<ol>bcda.insertBefore(<li>a, <li>b)"],
 	},
 	{
 		id: "move-mult-from-end-to-begin",
 		label: "Move multiple to beginning",
 		init: <List values={["c", "d", "e", "a", "b"]} />,
 		runs: <List values={["a", "b", "c", "d", "e"]} />,
+		idealDom: [
+			"<ol>cdeab.insertBefore(<li>a, <li>c)",
+			"<ol>acdeb.insertBefore(<li>b, <li>c)",
+		],
 	},
 	{
 		id: "move-one-to-end",
 		label: "Move one to end",
-		init: <List values={["b", "c", "d", "a"]} />,
+		init: <List values={["d", "a", "b", "c"]} />,
 		runs: <List values={["a", "b", "c", "d"]} />,
+		idealDom: ["<ol>dabc.insertBefore(<li>d, Null)"],
 	},
 	{
 		id: "move-mult-to-end",
 		label: "Move multiple to end",
 		init: <List values={["d", "e", "a", "b", "c"]} />,
 		runs: <List values={["a", "b", "c", "d", "e"]} />,
+		idealDom: [
+			"<ol>deabc.insertBefore(<li>d, Null)",
+			"<ol>eabcd.insertBefore(<li>e, Null)",
+		],
 	},
 	{
 		id: "simple-swap",
 		label: "Simple swap",
 		init: <List values={["b", "a"]} />,
 		runs: <List values={["a", "b"]} />,
+		idealDom: ["<ol>ba.insertBefore(<li>b, Null)"],
 	},
 	{
 		id: "swap-middle",
 		label: "Swap middle children",
 		init: <List values={["a", "c", "b", "d"]} />,
 		runs: <List values={["a", "b", "c", "d"]} />,
+		idealDom: ["<ol>acbd.insertBefore(<li>c, <li>d)"],
 	},
 	{
 		id: "reverse",
@@ -132,6 +152,17 @@ const operations = [
 				values={["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"].reverse()}
 			/>
 		),
+		idealDom: [
+			"<ol>abcdefghij.insertBefore(<li>j, <li>a)",
+			"<ol>jabcdefghi.insertBefore(<li>i, <li>a)",
+			"<ol>jiabcdefgh.insertBefore(<li>h, <li>a)",
+			"<ol>jihabcdefg.insertBefore(<li>g, <li>a)",
+			"<ol>jihgabcdef.insertBefore(<li>f, <li>a)",
+			"<ol>jihgfabcde.insertBefore(<li>e, <li>a)",
+			"<ol>jihgfeabcd.insertBefore(<li>d, <li>a)",
+			"<ol>jihgfedabc.insertBefore(<li>c, <li>a)",
+			"<ol>jihgfedcab.insertBefore(<li>a, Null)",
+		],
 	},
 ];
 
@@ -145,6 +176,7 @@ const recreateInput = /** @type {HTMLInputElement} */ (
 	document.getElementById("recreate")
 );
 const mutationList = document.getElementById("mutations");
+const idealMutationList = document.getElementById("ideal-mutations");
 
 function setupSelect() {
 	for (let operation of operations) {
@@ -168,11 +200,20 @@ function getSelectedOperation() {
 }
 
 function setupOperation() {
+	const selectedOp = getSelectedOperation();
+
+	idealMutationList.textContent = "";
+	for (let domOp of selectedOp.idealDom) {
+		const li = document.createElement("li");
+		li.textContent = domOp;
+		idealMutationList.appendChild(li);
+	}
+
 	if (recreateInput.checked) {
 		container = createContainer();
-		ReactDOM.render(getSelectedOperation().init, container);
+		ReactDOM.render(selectedOp.init, container);
 	} else {
-		ReactDOM.render(getSelectedOperation().init, container);
+		ReactDOM.render(selectedOp.init, container);
 	}
 }
 
@@ -208,6 +249,7 @@ runBtn.addEventListener("click", () => {
 	ReactDOM.render(getSelectedOperation().runs, container);
 	stopCapture();
 
+	mutationList.textContent = "";
 	for (let log of getLog()) {
 		const li = document.createElement("li");
 		li.textContent = log;
